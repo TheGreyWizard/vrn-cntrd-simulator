a3e100082e341af457282caa700dac4bbf9b3571
Megnath Ramesh <megnath@ualberta.ca>
2020-07-07 21:27:51 -0600

    Calculate the density function and plot in map

--- Simulator/__main__.py
+++ Simulator/__main__.py
@@ -10,15 +10,9 @@ import random
 polygon_coords = [(0,0), (2.125,0),(2.9325,1.5),(2.975,1.6),
 (2.9325,1.7),(2.295,2.1),(0.85,2.3),(0.17,1.2),(0,0)] #TODO: Make it customizable
 
+gaussian_centers = [(2.15,.75), (1.,.25), (.725,1.75), (.25,.7)]
+
 resolution = 0.01 #TODO: Use it
-
-environment_polygon = Path(polygon_coords) # make a polygon
-
-fig, ax = plt.subplots()
-patch = patches.PathPatch(environment_polygon, facecolor='None')
-ax.add_patch(patch)
-ax.set_xlim(-1, 4)
-ax.set_ylim(-1, 4)
 
 #Define agents
 
@@ -30,7 +24,7 @@ def generate_sample_points(polygon):
     resultx = []
     resulty = []
 
-    x, y = np.meshgrid(np.arange(0,3,0.01), np.arange(0,3,0.01))
+    x, y = np.meshgrid(np.arange(0,3,resolution), np.arange(0,3,resolution))
     x, y = x.flatten(), y.flatten()
     points = np.vstack((x,y)).T
 
@@ -44,22 +38,49 @@ def generate_sample_points(polygon):
     return resultx, resulty
 
 
-x, y = generate_sample_points(environment_polygon)
+def calculate_densityFunction(x_points, y_points, x_center, y_center):
+    phi = []
 
-robot_positions = []
-for i in range(no_robots):
-    index = random.randint(0,len(x))
-    robot_positions.append([x[index], y[index]])
+    for i in range(len(x_points)):
+        dist_x = (x_points[i] - x_center)
+        sqdist_x = pow(dist_x,2)
 
-x, y = zip(*robot_positions)
+        dist_y = (y_points[i] - y_center)
+        sqdist_y = pow(dist_y,2)
+        phi.append(np.exp(6*(- sqdist_x - sqdist_y)))
+    
+    return np.asarray(phi)
 
-plt.scatter(x,y)
 
-vor = Voronoi(robot_positions)
-vor_fig = voronoi_plot_2d(vor, show_vertices=False, line_colors='orange',
-                line_width=2, line_alpha=0.6, point_size=2)
+if __name__ == "__main__":
 
-def calculate_centroid(vornoi_vertices):
-    pass
+    environment_polygon = Path(polygon_coords) # make a polygon
 
-plt.show()
+    x, y = generate_sample_points(environment_polygon)
+
+    fig, ax = plt.subplots()
+    patch = patches.PathPatch(environment_polygon, facecolor='None')
+    ax.add_patch(patch)
+    ax.set_xlim(-1, 4)
+    ax.set_ylim(-1, 4)
+
+    robot_positions = []
+    for i in range(no_robots):
+        index = random.randint(0,len(x))
+        robot_positions.append([x[index], y[index]])
+    
+    all_phi = []
+    
+    for i in gaussian_centers:
+        all_phi.append(calculate_densityFunction(x,y,i[0],i[1]))
+
+    cumulative_phi = all_phi[0] #account for unimodal gaussian distribution
+    for i in range(1,len(all_phi)):
+        temp_phi = all_phi[i]
+        cumulative_phi = np.sum([cumulative_phi, temp_phi], axis=0)
+
+    robot_x, robot_y = zip(*robot_positions)
+
+    plt.scatter(x,y, c=cumulative_phi, s=100)
+
+    plt.show()
