d987d1afe03c8e1c8c1cf3db67250678af171e97
Megnath Ramesh <megnath@ualberta.ca>
2020-07-09 10:58:38 -0600

    Actually integrate along the curve instead of summing the values

--- Simulator/Control_Law.py
+++ Simulator/Control_Law.py
@@ -1,29 +1,33 @@
+from scipy import integrate
+
 def calculate_polygon_mass(polygon, points, density_vals):
 
     grid = polygon.contains_points(points)
-    mass = 0
+    polygon_density_vals = []
 
     for i in range(0,len(grid)):
             if(grid[i]):
-                mass = mass + density_vals[i]
+                polygon_density_vals.append(density_vals[i])
     
-    return mass
+    #integrate along the clipped density function
+    return integrate.simps(polygon_density_vals)
 
 def calculate_centroid(polygon, points, density_vals):
 
     mass = calculate_polygon_mass(polygon, points, density_vals)
     grid = polygon.contains_points(points)
 
-    centroidx = 0
-    centroidy = 0
+    x_density = []
+    y_density = []
 
     for i in range(0,len(grid)):
             if(grid[i]):
-                centroidx = centroidx + (points[i][0] * density_vals[i])
-                centroidy = centroidy + (points[i][1] * density_vals[i])
+                x_density.append(points[i][0] * density_vals[i])
+                y_density.append(points[i][1] * density_vals[i])
     
-    centroidx = centroidx / mass
-    centroidy = centroidy / mass
+    #integrate along the centroid calculation function
+    centroidx = integrate.simps(x_density) / mass
+    centroidy = integrate.simps(y_density) / mass
     
     return (centroidx, centroidy)
     
