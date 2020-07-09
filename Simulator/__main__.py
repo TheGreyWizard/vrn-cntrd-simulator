0285b7285ca592c0fdf16cb981dcc5c40d841dcf
Megnath Ramesh <megnath@ualberta.ca>
2020-07-09 00:02:45 -0600

    Clean up and start voronoi tesselection

--- Simulator/__main__.py
+++ Simulator/__main__.py
@@ -23,8 +23,6 @@ no_robots = 10
 #Define pdf
 
 def generate_sample_points(polygon):
-    resultx = []
-    resulty = []
     masked_points = []
 
     x, y = np.meshgrid(np.arange(0,3,resolution), np.arange(0,3,resolution))
@@ -35,19 +33,17 @@ def generate_sample_points(polygon):
 
     for i in range(0,len(grid)):
             if(grid[i]):
-                resultx.append(points[i][0])
-                resulty.append(points[i][1])
                 masked_points.append(points[i])
 
-    return masked_points, resultx, resulty
+    return masked_points
 
 
-def calculate_densityFunction(x_points, y_points, x_center, y_center):
+def calculate_densityFunction(points, x_center, y_center):
     phi = []
 
-    for i in range(len(x_points)):
-        sqdist_x = (x_points[i] - x_center)**2
-        sqdist_y = (y_points[i] - y_center)**2
+    for i in range(len(points)):
+        sqdist_x = (points[i][0] - x_center)**2
+        sqdist_y = (points[i][1] - y_center)**2
         phi.append(np.exp(6*(- sqdist_x - sqdist_y)))
     
     return np.asarray(phi)
@@ -56,7 +52,7 @@ if __name__ == "__main__":
 
     environment_polygon = Path(polygon_coords) # make a polygon
 
-    Q, x, y = generate_sample_points(environment_polygon)
+    Q = generate_sample_points(environment_polygon)
 
     fig, ax = plt.subplots()
     patch = patches.PathPatch(environment_polygon, facecolor='None')
@@ -66,24 +62,27 @@ if __name__ == "__main__":
 
     robot_positions = []
     for i in range(no_robots):
-        index = random.randint(0,len(x))
-        robot_positions.append([x[index], y[index]])
+        index = random.randint(0,len(Q))
+        robot_positions.append([Q[index][0], Q[index][1]])
     
     all_phi = []
     
     for i in gaussian_centers:
-        all_phi.append(calculate_densityFunction(x,y,i[0],i[1]))
+        all_phi.append(calculate_densityFunction(Q,i[0],i[1]))
 
     cumulative_phi = all_phi[0] #account for unimodal gaussian distribution
     for i in range(1,len(all_phi)):
         temp_phi = all_phi[i]
         cumulative_phi = np.sum([cumulative_phi, temp_phi], axis=0)
 
+    x, y = zip(*Q)
     robot_x, robot_y = zip(*robot_positions)
 
     centroid = calculate_centroid(environment_polygon, Q, cumulative_phi)
 
     plt.scatter(x,y, c=cumulative_phi)
     plt.plot([centroid[0]], [centroid[1]], marker='o', markersize=3, color="red")
+
+    vor = Voronoi(robot_positions)
 
     plt.show()
