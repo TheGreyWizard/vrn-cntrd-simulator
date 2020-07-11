1ba09975f48609f9d4e25cef4dc28c7cd2198830
Megnath Ramesh <megnath@ualberta.ca>
2020-07-10 21:30:12 -0600

    Start making this system object oriented

--- Simulator/Control_Law.py
+++ Simulator/Control_Law.py
@@ -1,34 +1,87 @@
 from scipy import integrate
+from scipy.spatial import Voronoi
+import numpy as np
 
-def calculate_polygon_mass1(polygon, points, density_vals, resolution):
+import matplotlib.pyplot as plt
 
-    grid = polygon.contains_points(points)
-    polygon_density_vals = []
+from shapely.geometry import Polygon
+from matplotlib.path import Path
 
-    for i in range(0,len(grid)):
-            if(grid[i]):
-                polygon_density_vals.append(density_vals[i])
-    
-    #integrate along the clipped density function
-    return integrate.simps(polygon_density_vals, dx=resolution)
+import random
 
-def calculate_centroid(polygon, points, density_vals, resolution):
+from FiniteVoronoiTesselection import voronoi_finite_polygons_2d
 
-    mass = calculate_polygon_mass1(polygon, points, density_vals, resolution)
+class SimulationController:
 
-    grid = polygon.contains_points(points)
+    robot_positions = []
+    no_iterations = None
+    Q = None
+    polygon = None
 
-    x_density = []
-    y_density = []
+    def __init__(self, polygon_coords, Q, no_robots, no_iterations):
+        
+        self.no_iterations = no_iterations
+        self.Q = Q
+        self.polygon = Polygon(polygon_coords)
 
-    for i in range(0,len(grid)):
-            if(grid[i]):
-                x_density.append(points[i][0] * density_vals[i])
-                y_density.append(points[i][1] * density_vals[i])
-    
-    #integrate along the centroid calculation function
-    centroidx = integrate.simps(x_density, dx=resolution) / mass
-    centroidy = integrate.simps(y_density, dx=resolution) / mass
-    
-    return (centroidx, centroidy)
-    
+        for i in range(no_robots):
+            index = random.randint(0,len(Q))
+            self.robot_positions.append([Q[index][0], Q[index][1]])
+
+    def run_simulation(self, cumulative_density, resolution):
+
+        for i in range(self.no_iterations):
+
+            vor = Voronoi(self.robot_positions)
+            regions, vertices = voronoi_finite_polygons_2d(vor)
+            robot_polygons = []
+
+            for index in range(len(regions)):
+                region = regions[index]
+                poly = Polygon(vertices[region])
+                # Clipping polygon
+                poly = poly.intersection(self.polygon)
+                polygon_vertices = [p for p in poly.exterior.coords]
+                robot_polygons.append(np.array(polygon_vertices))
+                polygon = Path(polygon_vertices)
+
+                centroid = self.calculate_centroid(polygon, self.Q, cumulative_density, resolution)
+
+                plt.plot([vor.points[index][0], centroid[0]], [vor.points[index][1], centroid[1]], color="yellow")
+
+                self.robot_positions[index] = centroid
+        
+        return self.robot_positions, robot_polygons
+
+    def calculate_polygon_mass(self, polygon, points, density_vals, resolution):
+
+        grid = polygon.contains_points(points)
+        polygon_density_vals = []
+
+        for i in range(0,len(grid)):
+                if(grid[i]):
+                    polygon_density_vals.append(density_vals[i])
+        
+        #integrate along the clipped density function
+        return integrate.simps(polygon_density_vals, dx=resolution)
+
+    def calculate_centroid(self, polygon, points, density_vals, resolution):
+
+        mass = self.calculate_polygon_mass(polygon, points, density_vals, resolution)
+
+        grid = polygon.contains_points(points)
+
+        x_density = []
+        y_density = []
+
+        for i in range(0,len(grid)):
+                if(grid[i]):
+                    x_density.append(points[i][0] * density_vals[i])
+                    y_density.append(points[i][1] * density_vals[i])
+        
+        #integrate along the centroid calculation function
+        centroidx = integrate.simps(x_density, dx=resolution) / mass
+        centroidy = integrate.simps(y_density, dx=resolution) / mass
+        
+        return (centroidx, centroidy)
+        
--- Simulator/__main__.py
+++ Simulator/__main__.py
@@ -1,13 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.path import Path
-from scipy.spatial import Voronoi, voronoi_plot_2d
 import numpy as np
-import random
-from shapely.geometry import Polygon
 
-from Control_Law import calculate_centroid
-from FiniteVoronoiTesselection import voronoi_finite_polygons_2d
+from Control_Law import SimulationController
 
 #Define Polygonal Environment
 
@@ -53,7 +49,6 @@ def calculate_densityFunction(points, x_center, y_center):
 if __name__ == "__main__":
 
     environment_polygon = Path(polygon_coords) # make a polygon
-    shapely_polygon = Polygon(polygon_coords)
 
     Q = generate_sample_points(environment_polygon)
 
@@ -62,48 +57,25 @@ if __name__ == "__main__":
     ax.add_patch(patch)
     ax.set_xlim(-1, 4)
     ax.set_ylim(-1, 4)
-
-    robot_positions = []
-    for i in range(no_robots):
-        index = random.randint(0,len(Q))
-        robot_positions.append([Q[index][0], Q[index][1]])
     
     all_phi = []
     
     for i in gaussian_centers:
         all_phi.append(calculate_densityFunction(Q,i[0],i[1]))
 
-    cumulative_phi = all_phi[0] #account for unimodal gaussian distribution
+    cumulative_density = all_phi[0] #account for unimodal gaussian distribution
     for i in range(1,len(all_phi)):
         temp_phi = all_phi[i]
-        cumulative_phi = np.sum([cumulative_phi, temp_phi], axis=0)
+        cumulative_density = np.sum([cumulative_density, temp_phi], axis=0)
 
     x, y = zip(*Q)
 
-    plt.scatter(x,y, c=cumulative_phi)
+    plt.scatter(x,y, c=cumulative_density)
 
     no_iterations = 100
 
-    for i in range(no_iterations):
-
-        vor = Voronoi(robot_positions)
-        regions, vertices = voronoi_finite_polygons_2d(vor)
-        robot_polygons = []
-
-        for index in range(len(regions)):
-            region = regions[index]
-            poly = Polygon(vertices[region])
-            # Clipping polygon
-            poly = poly.intersection(shapely_polygon)
-            polygon_vertices = [p for p in poly.exterior.coords]
-            robot_polygons.append(np.array(polygon_vertices))
-            polygon = Path(polygon_vertices)
-
-            centroid = calculate_centroid(polygon, Q, cumulative_phi, resolution)
-
-            plt.plot([vor.points[index][0], centroid[0]], [vor.points[index][1], centroid[1]], color="yellow")
-
-            robot_positions[index] = centroid
+    controller = SimulationController(polygon_coords, Q, no_robots, no_iterations)
+    robot_positions, robot_polygons = controller.run_simulation(cumulative_density, resolution)
     
     robot_x, robot_y = zip(*robot_positions)
     plt.scatter(robot_x, robot_y, marker='o', color='red')
